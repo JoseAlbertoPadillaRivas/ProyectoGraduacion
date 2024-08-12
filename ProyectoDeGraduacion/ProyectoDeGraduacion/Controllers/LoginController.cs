115eb01b2fdd04b33d2bd92c2046db83cfdb202c
Josu <josepadillarivas251004@gmail.com>
2024-08-11 19:42:03 -0600

    Primer aporte Jose Padilla
    Login, Registrarse, Index

---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Web.Mvc;
+
+namespace KN_Web.Controllers
+{
+    [OutputCache(NoStore = true, VaryByParam = "*", Duration = 0)]
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CerrarSesion()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+
+        [HttpGet]
+        public ActionResult Registrarse()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+
+    }
+}
---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoDeGraduacion.MvcApplication" Language="C#" %>
