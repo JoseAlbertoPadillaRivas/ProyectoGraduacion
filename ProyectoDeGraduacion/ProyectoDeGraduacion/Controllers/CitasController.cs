8c097dbd6e430e5d5aa83dc3a336e7ddd9d49540
Josueh7 <josueh070499@gmail.com>
2024-08-15 15:04:24 -0600

    Modulo Citas
    - Para acceder a la vista de administrador de las citas el URL es Citas/CitasProgramadas
    
    - Correcion link bootstrap

---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Controllers/CitasController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoDeGraduacion.Controllers
+{
+    public class CitasController : Controller
+    {
+        [HttpGet]
+        public ActionResult RegistrarCita()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult MisCitas()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CitasProgramadas()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ReprogramarCita()
+        {
+            return View();
+        }
+    }
+}
