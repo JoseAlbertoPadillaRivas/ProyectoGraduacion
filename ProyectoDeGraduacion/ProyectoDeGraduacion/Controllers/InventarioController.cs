0c5e30638a1742e9da6dc55a3d4ccdbf815c0645
Josu <josepadillarivas251004@gmail.com>
2024-08-14 21:17:32 -0600

    Aporte Jose Padilla

---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Controllers/InventarioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoDeGraduacion.Controllers
+{
+    public class InventarioController : Controller
+    {
+        // GET: Inventario
+        [HttpGet]
+        public ActionResult MostrarInventario()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ActualizarProducto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AgregarProducto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EliminarProducto()
+        {
+            return RedirectToAction("MostrarInventario", "Inventario");
+        }
+    }
+}
