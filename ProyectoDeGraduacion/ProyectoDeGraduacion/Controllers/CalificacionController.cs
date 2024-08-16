93cd8d995ded6c38c219c92f970924bbb8c69da1
Josueh7 <josueh070499@gmail.com>
2024-08-16 12:25:17 -0600

    Calificaciones (se debe completar)

---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Controllers/CalificacionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoDeGraduacion.Controllers
+{
+    public class CalificacionController : Controller
+    {
+        [HttpGet]
+        public ActionResult verCalificaciones()
+        {
+            return View();
+        }
+
+        public ActionResult NuevaCalificacion()
+        {
+            return View();
+        }
+
+        
+    }
+}
