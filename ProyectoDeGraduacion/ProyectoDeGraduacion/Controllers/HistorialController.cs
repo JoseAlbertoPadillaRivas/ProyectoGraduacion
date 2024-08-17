06018800cf4d98afca5fab02e5b55f3878434561
David Sanchez <davidfsjimenez@gmail.com>
2024-08-16 22:46:29 -0600

    Historial Clinico
    Se agregan las vistas del apartado de Historial Clinico

---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Controllers/HistorialController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoDeGraduacion.Controllers
+{
+    public class HistorialController : Controller
+    {
+        // GET: Historial
+        public ActionResult IndexHistorial()
+        {
+            return View();
+        }
+
+        // GET: VISTA PROVISIONAL
+        public ActionResult IndexHistorialAdmin()
+        {
+            return View();
+        }
+
+        // GET: Historial/RegistrarHistoria
+        public ActionResult RegistrarHistoria()
+        {
+            return View();
+        }
+
+        // GET: Historial/ModificarHistoria
+        public ActionResult ModificarHistoria()
+        {
+            return View();
+        }
+
+        // GET: Historial/AñadirDocumentos
+        public ActionResult AñadirDocumentos()
+        {
+            return View();
+        }
+
+        // GET: Historial/VerHistoria
+        public ActionResult VerHistoria()
+        {
+            return View();
+        }
+
+        // GET: Historial/DescargarDocumentos
+        public ActionResult DescargarDocumentos()
+        {
+            return View();
+        }
+    }
+}
