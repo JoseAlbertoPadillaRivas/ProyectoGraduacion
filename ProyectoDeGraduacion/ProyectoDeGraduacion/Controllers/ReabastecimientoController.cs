4cae7e09c4da9b771e4775a01e4375ab112f2cd8
David Sanchez <davidfsjimenez@gmail.com>
2024-08-19 17:48:33 -0600

    Modulo Reabastecimiento y Cambios Generales
    Se hicieron cambios generales en el diseno, y se agrego el modulo de reabastecimiento.

---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Controllers/ReabastecimientoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoDeGraduacion.Controllers
+{
+    public class ReabastecimientoController : Controller
+    {
+        // Vista principal del módulo de Reabastecimiento
+        public ActionResult IndexReabastecimiento()
+        {
+            return View();
+        }
+
+        // Vista para Configurar Valores de Reabastecimiento
+        public ActionResult ConfigurarValores()
+        {
+            return View();
+        }
+
+        // Vista para Generar Órdenes de Compra
+        public ActionResult GenerarOrdenes()
+        {
+            return View();
+        }
+
+        // Vista para Confirmar Recepción de Productos
+        public ActionResult ConfirmarRecepcion()
+        {
+            return View();
+        }
+
+        // Vista para el Historial de Reabastecimientos
+        public ActionResult HistorialReabastecimiento()
+        {
+            return View();
+        }
+    }
+}
