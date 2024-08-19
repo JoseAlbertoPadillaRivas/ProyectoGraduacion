04303840059065f7c49ba158a3119d04361c1404
Josu <josepadillarivas251004@gmail.com>
2024-08-18 22:23:08 -0600

    Seguimiento de productos y usuarios

---
+++ ProyectoDeGraduacion/ProyectoDeGraduacion/Controllers/SeguimientoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoDeGraduacion.Controllers
+{
+    public class SeguimientoController : Controller
+    {
+        [HttpGet]
+        public ActionResult MostrarProductos()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AgregarProducto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ActualizarProducto()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult MisProductos()
+        {
+            return View();
+        }
+
+    }
+}
